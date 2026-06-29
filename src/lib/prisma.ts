eb46be89951c578849f118bc7b024232aa3c2688
Shivaram-9 <ymvshiva1784@gmail.com>
2026-06-29 17:37:21 +0530

    fix: hardcode database URL in prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,6 +4,12 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient();
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: "postgresql://neondb_owner:npg_0bdtJc8rHgxQ@ep-empty-dawn-ahzuhi5v.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require"
+      }
+    }
+  });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
